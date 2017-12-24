90022b96ac3e8a9dbb823421aebb79e185401e0b
Hendri Pretorius <pretorh@gmail.com>
2017-12-24 10:24:29 +0200

    log the current token in MainActivity

--- app/src/main/java/org/pretorh/example/firebasetest/MainActivity.kt
+++ app/src/main/java/org/pretorh/example/firebasetest/MainActivity.kt
@@ -2,11 +2,21 @@ package org.pretorh.example.firebasetest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "current token: ${FirebaseInstanceId.getInstance()?.token}")
+    }
+
+    companion object {
+        val TAG = "MainActivity"
     }
 }
